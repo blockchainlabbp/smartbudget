e1dce177ba9eb7f73cfa3defeca5d4ae38e64f35
baltasarp <baltasarpasci@gmail.com>
2018-01-23 22:35:38 +0100

    tree data structure v1 (with minimal GUI to debug), removed metacoin example

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -14,10 +14,10 @@ import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract'
 
 // Import our contract artifacts and turn them into usable abstractions.
-import metacoin_artifacts from '../../build/contracts/MetaCoin.json'
+import datastruct_artifacts from '../../build/contracts/DataStructure.json'
 
-// MetaCoin is our usable abstraction, which we'll use through the code below.
-var MetaCoin = contract(metacoin_artifacts);
+// DataStructure is our usable abstraction, which we'll use through the code below.
+var DataStructure = contract(datastruct_artifacts);
 
 // The following code is simple to show off interacting with your contracts.
 // As your needs grow you will likely need to change its form and structure.
@@ -66,10 +66,15 @@ window.App = {
   start: function() {
     var self = this;
 
+<<<<<<< HEAD
+    // Bootstrap the DataStructure abstraction for Use.
+    DataStructure.setProvider(web3.currentProvider);
+=======
     self.renderContracts();
 
     // Bootstrap the MetaCoin abstraction for Use.
     MetaCoin.setProvider(web3.currentProvider);
+>>>>>>> a1c84dbb0e3dd1cd136ab3f3a20875069c4f7231
 
     // Get the initial account balance so it can be displayed.
     web3.eth.getAccounts(function(err, accs) {
@@ -85,8 +90,6 @@ window.App = {
 
       accounts = accs;
       account = accounts[0];
-
-      self.refreshBalance();
     });
   },
 
@@ -95,40 +98,113 @@ window.App = {
     status.innerHTML = message;
   },
 
-  refreshBalance: function() {
-    var self = this;
+  setError: function(message) {
+    var status = document.getElementById("error");
+    status.innerHTML = message;
+  },
 
+
+  addRoot: function() {
+    var self = this;
     var meta;
-    MetaCoin.deployed().then(function(instance) {
+
+    var stake = document.getElementById('stake_input').value;
+    var desc = document.getElementById('desc_input').value;
+
+    DataStructure.deployed().then(function(instance) {
       meta = instance;
-      return meta.getBalance.call(account, {from: account});
+      return meta.addRoot.sendTransaction(stake, desc, {from: account, gas:600000});
     }).then(function(value) {
-      var balance_element = document.getElementById("balance");
-      balance_element.innerHTML = value.valueOf();
+      self.setStatus("Tx: " + value.valueOf());
     }).catch(function(e) {
       console.log(e);
-      self.setStatus("Error getting balance; see log.");
+      self.setError("Error addRoot: </br>" + e);
+    });
+  }, 
+
+  addChild: function() {
+    var self = this;
+    var meta;
+
+    var stake = document.getElementById('child_stake_input').value;
+    var desc = document.getElementById('child_desc_input').value;
+    var parentid = document.getElementById('child_parentid_select').value;
+
+    DataStructure.deployed().then(function(instance) {
+      meta = instance;
+      return meta.addChild.sendTransaction(stake, desc, parentid, {from: account, gas:300000});
+    }).then(function(value) {
+      self.setStatus("Tx: " + value.valueOf());
+    }).catch(function(e) {
+      console.log(e);
+      self.setError("Error addRoot: </br>" + e);
+    });
+  }, 
+
+  getNodes: function() {
+    var self = this;
+    var meta;
+
+    DataStructure.deployed().then(function(instance) {
+      meta = instance;
+      return meta.getNodes.call({from: account, gas:500000});
+    }).then(function(value) {
+
+      var table = document.getElementById("nodes_table");
+
+      for(var i = 0; i < value[0].length; i++){
+        var row = table.insertRow(1);
+        var cell0 = row.insertCell(0);
+        var cell1 = row.insertCell(1);
+        var cell2 = row.insertCell(2);
+  
+        cell0.innerHTML = value[0][i];
+        cell1.innerHTML = value[1][i];
+        cell2.innerHTML = value[2][i];
+      }
+    }).catch(function(e) {
+      console.log(e);
+      self.setError("Error getNodes: </br>" + e);
     });
   },
 
-  sendCoin: function() {
+  getNodeDesc: function() {
     var self = this;
-
-    var amount = parseInt(document.getElementById("amount").value);
-    var receiver = document.getElementById("receiver").value;
-
-    this.setStatus("Initiating transaction... (please wait)");
-
     var meta;
-    MetaCoin.deployed().then(function(instance) {
+
+    DataStructure.deployed().then(function(instance) {
       meta = instance;
-      return meta.sendCoin(receiver, amount, {from: account});
-    }).then(function() {
-      self.setStatus("Transaction complete!");
-      self.refreshBalance();
+      return meta.getNodeDesc.call(0, {from: account, gas:300000});
+    }).then(function(value) {
+      self.setStatus(value.valueOf());
     }).catch(function(e) {
       console.log(e);
-      self.setStatus("Error sending coin; see log.");
+      self.setError("Error getNodes: </br>" + e);
+    });
+  }, 
+
+  getIds: function() {
+    var self = this;
+    var meta;
+
+    DataStructure.deployed().then(function(instance) {
+      meta = instance;
+      return meta.getIds.call({from: account, gas:300000});
+    }).then(function(value) {
+      var select = document.getElementById("child_parentid_select");
+
+      select.options.length = 0;
+
+      for(var i = 0; i < value[0].length; i++) {
+        var opt = document.createElement("option");
+        opt.value = value[i];
+        opt.textContent = value[i];
+        select.appendChild(opt);
+      }
+      
+    }).catch(function(e) {
+      console.log(e);
+      self.setError("Error getIds: </br>" + e);
     });
   },
 
@@ -186,18 +262,19 @@ window.App = {
 
     
   }
+
 };
 
 window.addEventListener('load', function() {
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
+    console.warn("Using web3 detected from external source. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
     // Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider);
   } else {
     console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
   }
 
   App.start();
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
+//var SmartBudget = artifacts.require("./SmartBudget.sol");
+var DataStructure = artifacts.require("./DataStructure.sol");
+
+module.exports = function(deployer) {
+  //deployer.deploy(SmartBudget);
+  deployer.deploy(DataStructure);
+=======
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
 var SmartBudget = artifacts.require("./SmartBudget.sol");
@@ -8,4 +16,5 @@ module.exports = function(deployer) {
   deployer.deploy(MetaCoin);
   // deploy a contract that will be locked for 1000 seconds after it has made it into the blockchain
   deployer.deploy(SmartBudget, 1000, 1, {value: web3.toWei(1.0, 'ether')});
+>>>>>>> a1c84dbb0e3dd1cd136ab3f3a20875069c4f7231
 };
