65568683dabcf0feb44d272ffe5199fffb6015f6
hb4r <hb4r00@gmail.com>
2018-04-17 23:33:43 +0200

    Defined test cases for the remaining features

--- test/TestSmartBudgetConstructor.js
+++ test/TestSmartBudgetConstructor.js
@@ -1,6 +1,16 @@
 var SmartBudget = artifacts.require("./SmartBudget.sol");
+var chai = require('chai');
+chai.use(require('chai-as-promised'));
+var expect = chai.expect;
+var assert = chai.assert; 
 // Some help on the available functions:
 // https://github.com/trufflesuite/truffle-contract
+
+function sleep(seconds) 
+{
+  var e = new Date().getTime() + (seconds * 1000);
+  while (new Date().getTime() <= e) {}
+}
 
 // http://truffleframework.com/docs/getting_started/javascript-tests#use-contract-instead-of-describe-
 contract('SmartBudget:ConstructorTests', function(accounts) {
--- test/TestSmartBudgetWithdrawal.js
+++ test/TestSmartBudgetWithdrawal.js
@@ -1,8 +1,32 @@
 var SmartBudget = artifacts.require("./SmartBudget.sol");
+var chai = require('chai');
+chai.use(require('chai-as-promised'));
+var expect = chai.expect;
+var assert = chai.assert; 
 // Some help on the available functions:
 // https://github.com/trufflesuite/truffle-contract
 
+function sleep(seconds) 
+{
+  var e = new Date().getTime() + (seconds * 1000);
+  while (new Date().getTime() <= e) {}
+}
+
 // http://truffleframework.com/docs/getting_started/javascript-tests#use-contract-instead-of-describe-
 contract('SmartBudget:WithdrawalTests', function(accounts) {
-  
+    it("should allow child_acc to withdraw its stake after completion", function() {
+    
+    });
+    it("should not allow withdrawal in TENDER period", function() {
+    
+    });
+    it("should not allow child_acc to withdraw its stake if in APPROVED state", function() {
+    
+    });
+    it("should allow root to withdraw the stake allocated for child if in APPROVED state", function() {
+    
+    });
+    it("should not allow double withdraw", function() {
+    
+    });
 });
