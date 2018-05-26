ee3c529af6c6773e534628b73ec5d1d36704254b
hb4r <hb4r00@gmail.com>
2018-05-26 13:58:48 +0200

    Started working on exposing functions to JS

--- app/javascripts/smartbudgetservice.js
+++ app/javascripts/smartbudgetservice.js
@@ -51,16 +51,22 @@ export const SmartBudgetService = {
     }
 };
 
-
+/**
+ * Main JS wrapper object to interact with the Ethereum network.
+ * Calls the interal truffle-contract instance's functions
+ * Verifies inputs
+ * Parses outputs
+ */
 function SmartBudgetInstance(instance)  {
 
     /**
-     * This is the contract instance, which has an address
+     * This is the truffle-contract instance, which already has an address
      */
     this.instance = instance;
 
+    //------------------------------------- Events ---------------------------------------------
     /**
-     * Watcher objects
+     * Events & watch callback setters
      */
     this.addNodeEvent = this.instance.SBNodeAdded();
     this.addCandidateEvent = this.instance.SBCandidateAdded();
@@ -95,8 +101,143 @@ function SmartBudgetInstance(instance)  {
         this._completedNodeEvent.watch(cb);  
     };
 
+    //------------------------------------- Validators ------------------------------------------
+    // Wraps any smart contract function an transforms revert to failed, and successful execution to passed
+    async function testWrapper (testPromise) {
+        try {
+            await testPromise;
+            return "passed";
+          } catch(err) {
+            return "failed";
+          }
+    };
+    
     /**
-     * The recursive function that gets the details for the nodes
+     * Validates nodeId
+     */
+    this.validateNodeId = async function(nodeId) {
+        return await testWrapper(this.instance.validateNodeId(nodeId));
+    };
+
+    /**
+     * Validates validateCandidateId
+     */
+    this.validateCandidateId = async function(candidateId) {
+        return await testWrapper(this.instance.validateCandidateId(candidateId));
+    };
+
+    /**
+     * Requires a specific contract state
+     */
+    this.requireContractState = async function(state) {
+        return await testWrapper(this.instance.requireContractState(state));
+    };
+
+    /**
+     * Requires the message sender to be the node's owner
+     */
+    this.requireNodeOwner = async function(fromAddress, nodeId) {
+        return await testWrapper(this.instance.requireNodeOwner(nodeId, {from: fromAddress}));
+    };
+
+    /**
+     * Requires the message sender to be the owner of the node's parent
+     */
+    this.requireNodeParentOwner = async function(fromAddress, nodeId) {
+        return await testWrapper(this.instance.requireNodeParentOwner(nodeId, {from: fromAddress}));
+    };
+
+    /**
+     * Validates that the node has enough stake to cover the proposed amount in the subcontract
+     */
+    this.validateStake = async function(stake, nodeId) {
+        return await testWrapper(this.instance.validateStake(web3.toWei(stake,'ether'), nodeId));
+    };
+
+    //------------------------------------- Getters ---------------------------------------------
+    /**
+     * Get contract state
+     */
+    this.getContractState = async function() {
+        var state = await this.instance.getContractState();
+        /*
+        * ContractState enum disrcibes the state of the smartbudget contract.
+        * INVALID   - 0 - Before initalization is complete
+        * CANCELLED - 1 - Some first-level subprojects of the main project have not been approved
+        * TENDER    - 2 - Contract is in the tender period
+        * DELIVERY  - 3 - Contract is in delivery period
+        * FINISHED  - 4 - Delivery time lock has expired, withdraw is opened
+        */
+        switch (state.toString()) {
+            case "0":
+                return "INVALID";
+            case "1":
+                return "CANCELLED";
+            case "2":
+                return "TENDER";
+            case "3":
+                return "DELIVERY";
+            case "4":
+                return "FINISHED";
+            default:
+                throw new Error("JS function getContractState is not compatible with smart contract version!");
+        };
+    };
+
+    /**
+     * Parse node state
+     */
+    function parseNodeState(stateId) {
+       /*
+        * NodeState enum disrcibes the state of nodes.
+        * OPEN       - 0 - The node has been created but has not been approved by the parent. Candidates may apply for the node
+        * APPROVED   - 1 - Parent node has approved one of the candidates
+        * COMPLETED  - 2 - Root has accepted the delivered work, 
+        *                  promised stake will be withdrawable by the node owner 
+        *                  after the delivery time lock has expired
+        * PAYED      - 3 - Owner of the node has alreay withdrawn its stake from the contract
+        */
+        switch (stateId.toString()) {
+            case "0":
+                return "OPEN";
+            case "1":
+                return "APPROVED";
+            case "2":
+                return "COMPLETED";
+            case "3":
+                return "PAYED";
+            default:
+                throw new Error("JS function getNodeState is not compatible with smart contract version!");
+        };
+    };
+
+    /**
+     * GetNodeWeb wrapper
+     */
+    this.getNodeWeb = async function(nodeId) {
+        /** "stake" : "Stake of node",
+        *   "addr" : "Address of node",
+        *   "state" : "State of node",
+        *   "cands" : "Array of candidate ids",
+        *   "desc" : "Description of node",
+        *   "parent" : "Id of parent node",
+        *   "childs" : "Array of child node ids"
+        */
+        var attributes = await this.instance.getNodeWeb(nodeId, {gas: 500000 });
+        var smartNode = {id: nodeId, 
+            stakeInEth: web3.fromWei(attributes[0], "ether").toNumber(),
+            address: attributes[1].toString(),
+            state: [attributes[2]].map((stateId) => parseNodeState(stateId))[0],
+            candidateIds: attributes[3].map((id) => id.toNumber()),
+            name: attributes[4].toString(),
+            parentId: attributes[5].toNumber(),
+            childIds: attributes[6].map((id) => id.toNumber())};
+        return smartNode;
+    }
+
+
+    /**
+     * The recursive function that gets the details of the nodes
      */
     this.visitNode = async function (nodeId, currDepth, maxDepth) {
         // Get node by ID    
@@ -146,6 +287,8 @@ function SmartBudgetInstance(instance)  {
         return subTree;
     };
 
+    //------------------------------------- Updaters ---------------------------------------------
+
     /**
      * Create contractors
      */
@@ -164,17 +307,24 @@ function SmartBudgetInstance(instance)  {
     */
    this.addNode = async function (fromAddress, description, parentId) {
         console.log("Called contract.addNode with parmeters: (" + fromAddress + "," + description + "," + parentId + ")");
-        /** Do input checks here */
-        var res = await this.instance.addNode(description, parentId, {from: fromAddress});
-        console.log("Awaited addNode, the returned log is " + JSON.stringify(res.logs));
-        /** Parse logs and select the relevant one here */
-        return res.logs[0].args;    
+        if (await this.requireContractState(2) != "passed") {
+            var actualState = await this.getContractState();
+            alert("Cannot add new node, contract is required " +
+                "to be in TENDER state, but the actual state is " + actualState);
+        } else if (await this.validateNodeId(parentId) != "passed") {
+            alert("Error: parent id " + parentId + " is not a valid id!");
+        }else if (await this.requireNodeOwner(fromAddress, parentId) != "passed") {
+            var node = await this.getNodeWeb(parentId);
+            console.log("Node: " + JSON.stringify(node));
+            alert("Cannot add new node, because you're not the owner of node " + 
+                node.name + " (id: " + node.id + ")");
+        } else {
+            var res = await this.instance.addNode(description, parentId, {from: fromAddress});
+            console.log("Awaited addNode, the returned log is " + JSON.stringify(res.logs));
+            /** Parse logs and select the relevant one here */
+            return res.logs[0].args;    
+        }
     };
-
-    /*
-    *  To create an async env in a sync func: (async () => { statements })();
-    */
-
 
     /**
      * Wrapper for SmartBudget.applyForNode(uint nodeId, string name, uint stake)
